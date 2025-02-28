b3b74dd916304e3d735c4fe565431c5d0bb6204e
abhishekptll <abhishekptl9407@gmail.com>
2025-02-28 16:33:23 +0530

    srgfds

---
+++ test.js
@@ -0,0 +1,29 @@
+let a= "abhishek";
+console.log(a);
+
+let b = 20;
+let c = 30;
+
+let d = b;
+b = c;
+c = d;
+
+console.log("b =",b, "  c = " ,c);
+
+
+let n = parseInt(prompt("enter a no"))
+
+if(n%2==0){
+    console.log("even");
+}else{
+    console.log("odd");
+}
+
+
+let user = parseInt(prompt("enter a no"))
+
+if( user >=18){
+    console.log("eligible to vote");
+}else{
+    console.log("not eligible to vote");
+}
