7b5316e8c34d6145969a0b4a835dfca0644f1178
abhishekptll <abhishekptl9407@gmail.com>
2025-03-20 15:39:52 +0530

    dfgfdgf

--- splice.js
+++ splice.js
@@ -1,4 +1,10 @@
 let arr = [1,"cybrom" , 26 , 'a'];
 
-arr.splice(2,1, "abhishek");
-console.log(arr);
+// arr.splice(2,1, "abhishek");
+// console.log(arr);
+
+let arr2= ["cybrom", 26, "milan" , 46];
+// console.log(shop);
+
+let arr3 =[...arr,...arr2];
+console.log(arr3);
